ccc4aab82d2e3b008c19a6820733c2537bc9d04e
Juan Guillherme <130096510+DEV-JUANguillherme@users.noreply.github.com>
2024-04-26 15:02:50 -0300

    .

--- back/src/services/user/CreateUserServices.ts
+++ back/src/services/user/CreateUserServices.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
 
 interface UserRequest{
   name: string;
@@ -23,6 +24,7 @@ class CreateUserService{
       throw new Error("User already exists")
     }
 
+    const passwordHash = await hash(password, 8)
     const user = await prismaClient.user.create({
       data:{
         name:name,
@@ -31,7 +33,7 @@ class CreateUserService{
       }
     })
 
-
+    
     return user
   }
 }
