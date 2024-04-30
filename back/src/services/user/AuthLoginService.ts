b4050937e83d795e085266b24454c0bff7d957e1
Juan Guillherme <130096510+DEV-JUANguillherme@users.noreply.github.com>
2024-04-30 11:07:12 -0300

    fazendo verificações de email e senha incorretas.

--- back/src/services/user/AuthLoginService.ts
+++ back/src/services/user/AuthLoginService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
 
 interface AuthResquest{
     email: string,
@@ -6,7 +7,21 @@ interface AuthResquest{
 }
 class AuthUserService{
     async execute({email, password}: AuthResquest){
-        
+        // verificar se o email existe
+        const user = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+        if (!user){
+            throw new Error("User/password incorrect")
+        }
+        // verificar se a senha esta correta
+        const passwordMatch = await compare(password, user.password)
+
+        if(!passwordMatch){
+            throw new Error("User/password incorrect")
+        }
     }
 }
 
