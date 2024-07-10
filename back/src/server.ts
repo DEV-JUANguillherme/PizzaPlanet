9f9fd782a6bc3c0c35f8637de39c6de61979fb47
Juan Guillherme <130096510+DEV-JUANguillherme@users.noreply.github.com>
2024-07-10 20:01:41 -0300

    m

--- back/src/server.ts
+++ back/src/server.ts
@@ -17,7 +17,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
       error: err.message
     })
   }
-
+   
   return res.status(500).json({
     status: 'error',
     message: 'Internal server error.'
